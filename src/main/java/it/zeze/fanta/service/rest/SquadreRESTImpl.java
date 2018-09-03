adb4850f9767ef305f3b3b8bed181635f55875a3
enrico <enrico.zeranto@gmail.com>
2018-09-03 20:29:08 +0200

    Add FantaFormazioneEJB and remove it from WebService

--- src/main/java/it/zeze/fanta/service/rest/SquadreRESTImpl.java
+++ src/main/java/it/zeze/fanta/service/rest/SquadreRESTImpl.java
@@ -1,22 +1,17 @@
 package it.zeze.fanta.service.rest;
 
-import java.util.List;
+import it.zeze.fanta.service.definition.SquadreInterface;
+import it.zeze.fanta.service.definition.ejb.SquadreLocal;
+import it.zeze.fantaformazioneweb.entity.Squadre;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-import it.zeze.fanta.db.DBManager;
-import it.zeze.fanta.service.definition.SquadreInterface;
-import it.zeze.fanta.service.definition.ejb.SquadreLocal;
-import it.zeze.fantaformazioneweb.entity.Squadre;
 
 @Path("/squadreRESTImpl")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -25,9 +20,6 @@ import it.zeze.fantaformazioneweb.entity.Squadre;
 public class SquadreRESTImpl implements SquadreInterface {
 
 	private static final Logger logger = LogManager.getLogger(SquadreRESTImpl.class);
-	
-	@EJB(name = "DBManager")
-	private DBManager dbManager;
 	
 	@EJB(name = "SquadreEJB")
 	private SquadreLocal squadreEJB;
